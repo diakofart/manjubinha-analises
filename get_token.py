53333011752e53e97585a27fffef7e7b7b7f88e4
diakofart <diakofart@gmail.com>
2026-06-25 11:05:24 -0300

    Update get_token.py

--- get_token.py
+++ get_token.py
@@ -6,7 +6,7 @@ resp = requests.post(
         "client_id": "142632",
         "client_secret": "3VUIzQ0LtdCBrFYjFx16YJyPb6Yw486IjgiwTshHIdN9BKrgLiXAeW35NghvdRDr",
         "redirect_uri": "https://diakofart.github.io/manjubinha-analises/callback.html",
-        "code": "uyCPrrOIgr",
+        "code": "Dk4EG9T1St&state",
         "grant_type": "authorization_code"
     }
 )
