8221f952e8146b11c97aca79ab85178634374fa4
diakofart <diakofart@gmail.com>
2026-06-25 10:58:00 -0300

    get_token

---
+++ get_token.py
@@ -0,0 +1,13 @@
+import requests
+
+resp = requests.post(
+    "https://public-api.wordpress.com/oauth2/token",
+    data={
+        "client_id": "142632",
+        "client_secret": "3VUIzQ0LtdCBrFYjFx16YJyPb6Yw486IjgiwTshHIdN9BKrgLiXAeW35NghvdRDr",
+        "redirect_uri": "https://diakofart.github.io/manjubinha-analises/callback.html",
+        "code": "uyCPrrOIgr",
+        "grant_type": "authorization_code"
+    }
+)
+print(resp.json())
